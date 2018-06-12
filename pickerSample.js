a334fa14c874d98adb2c24dead715e21500b4bf4
thomaswongGit <40166179+thomaswongGit@users.noreply.github.com>
2018-06-13 01:21:46 +0800

    Update pickerSample.js

--- pickerSample.js
+++ pickerSample.js
@@ -1,7 +1,10 @@
+<View>
+<Text>get Changed: {this.state.language}</Text>
 <Picker
   selectedValue={this.state.language}
-  style={{ height: 50, width: 100 }}
+  style={{ position:'absolute',bottom:0,left:0,right:0 }}
   onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
   <Picker.Item label="Java" value="java" />
   <Picker.Item label="JavaScript" value="js" />
 </Picker>
+</View>
